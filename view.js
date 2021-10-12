e668144fb3420b56aff9064fcbc4e0ed11345f57
45675 <90397041+45675@users.noreply.github.com>
2021-10-12 11:00:37 -0500

    Update view.js

--- view.js
+++ view.js
@@ -1,12 +1,12 @@
 function view() {
   this.update = function() {
     this.updatelevel();
-  }
+  };
   this.updatelevel = function() {
     document.getElementById('level').innerHTML = intToString(main.level);
     document.getElementById('xp').innerHTML = intToString(main.xp);
     document.getElementById('xpneeded').innerHTML = intToString(main.xpneeded);
-  }
+  };
   this.updateHealth = function() {
     document.getElementById('HP').innerHTML = intToString(main.HP);
     document.getElementById('MaxHP').innerHTML = intToString(main.MaxHP);
