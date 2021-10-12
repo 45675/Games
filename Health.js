20742193a768de719324b5ccccacc88805ec42f6
45675 <90397041+45675@users.noreply.github.com>
2021-10-12 09:44:02 -0500

    Create Health.js

---
+++ Health.js
@@ -0,0 +1,12 @@
+function health() {
+  this.HP = 0;
+  this.MaxHP= 0;
+  this.excess = 0;
+  this.tick = function(change) {
+    this.HP = this.hp + change;
+    this.excess = this.HP - this.MaxHP
+    if (this.excess > 0) {
+      this.HP = this.MaxHP;
+    }
+  }
+}
